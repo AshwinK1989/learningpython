d5f74f41c593f1691fb1b28679da476c985c2485
Ashwin Karangutkar <ashwin.karangutkar@gmail.com>
2018-09-09 14:28:41 +0530

    String Properties and Methods

---
+++ singlefiles/stringpropertiesmethod.py
@@ -0,0 +1,8 @@
+msg ="Hello World"
+print("Printing message  in lower case "+msg.lower())
+
+print("Printing message in upper case "+msg.upper())
+
+print("Spliting the string into list: ")
+print(msg.split())
+
