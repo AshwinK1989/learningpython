78882cf7a00498df2cc77ad094f7c43b25c923bb
Ashwin Karangutkar <ashwin.karangutkar@gmail.com>
2018-09-10 16:16:37 +0530

    Tuple

---
+++ singlefiles/tuple.py
@@ -0,0 +1,17 @@
+my_tuple = ('Ashwin',1,2,'Karangutkar','Ashwin','Karangutkar')
+
+#Extracting first value from tuple using indexing
+print("The first value from Tuple is :")
+print(my_tuple[0])
+
+
+#Extracting value from 1st position till the end
+print("Extracting value from first position till end")
+print(my_tuple[1:])
+
+#Getting the number of times a element is present
+print(my_tuple.count('Ashwin'))
+
+#Getting the index at which an element is present
+print(my_tuple.index('Ashwin'))
+
