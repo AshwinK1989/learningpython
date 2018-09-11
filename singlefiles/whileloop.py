2ecbf6debb5c289c6a81fd1a3f78f126b8ea3d8f
Ashwin Karangutkar <ashwin.karangutkar@gmail.com>
2018-09-11 22:23:15 +0530

    While loops

--- singlefiles/whileloop.py
+++ singlefiles/whileloop.py
@@ -4,7 +4,7 @@ while num<5:
 	print "The num is", num
 	num=num+1
 else:
-	 print('Loop ended')	
+	 print('Loop ended')    
 
 #Usage of break statement
 
@@ -23,6 +23,14 @@ while num<5:
 		continue
 	
 	print "The num is ",num
+
+#Usage of pass, it is used when we want to implement something which is not ready and to avoid errors
+num=0
+while num==1:
+	#comment
+	pass
+
+
 	
 	
 
