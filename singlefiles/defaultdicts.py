eddd963bd8fa12380e26db52331122b16ab99289
Ashwin Karangutkar <ashwin.karangutkar@gmail.com>
2018-09-24 18:28:14 +0530

    Default dict

--- singlefiles/defaultdicts.py
+++ singlefiles/defaultdicts.py
@@ -6,8 +6,11 @@ d={'k1':1}
 print(d['k1'])
 
 list1=[1,2,3,4,5]
-d=defaultdict(lambda:list1)
+list2=[6,6,8,9]
 
-print(d['k2'])
+d=defaultdict(lambda:list1)
+d=defaultdict(lambda:list2)
+
+print(d['k3'])
 
 print(d)
