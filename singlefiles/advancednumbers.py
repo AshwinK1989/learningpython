2975b85cbfa006fdca3fd4b31e30945575cbb61b
Ashwin Karangutkar <ashwin.karangutkar@gmail.com>
2018-09-28 11:52:47 +0530

    Advanced number

---
+++ singlefiles/advancednumbers.py
@@ -0,0 +1,18 @@
+#Convert a number to it's hexadecimal:
+
+print(hex(10))
+
+#Convert a number to it's binary value:
+print(bin(20))
+
+#Use pow function to perform 2**4:
+print(pow(2,4))
+
+#Use pow function to perform 2**4 and perform mod operation on it using the third argument
+print(pow(2,4,4))
+
+#Provide absolute value of the number:
+print(abs(-10))
+
+#Provide the round value of a number, it will return a float
+print(round(14.99))
